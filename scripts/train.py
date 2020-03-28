93a338c54f9ab59d725940fb08bf71ea73448fff
Naoya Yamaguchi <708yamaguchi@gmail.com>
2020-03-29 00:43:10 +0900

    reduce args in argparse for readability and usability

--- scripts/train.py
+++ scripts/train.py
@@ -8,11 +8,11 @@ import argparse
 import chainer
 from chainer import dataset
 from chainer import training
-from chainer.training import extensions
+from chainer.links import VGG16Layers
+import chainer.backends.cuda
 from chainer.serializers import npz
+from chainer.training import extensions
 
-# from jsk_recognition_utils.chainermodels import VGG16BatchNormalization
-# from jsk_recognition_utils.chainermodels import VGG16
 from vgg16.vgg16_batch_normalization import VGG16BatchNormalization as VGG16
 
 import matplotlib
@@ -42,92 +42,48 @@ class PreprocessedDataset(chainer.dataset.DatasetMixin):
 
 
 def main():
-    archs = {  # only VGG16 is available now
-        'vgg16': VGG16,
-        # 'alex': alex.Alex,
-        # 'googlenet': googlenet.GoogLeNet,
-        # 'googlenetbn': googlenetbn.GoogLeNetBN,
-        # 'nin': nin.NIN,
-        # 'resnet50': resnet50.ResNet50,
-        # 'resnext50': resnext50.ResNeXt50,
-    }
-
-    dtypes = {
-        'float16': np.float16,
-        'float32': np.float32,
-        'float64': np.float64,
-    }
     rospack = rospkg.RosPack()
 
     parser = argparse.ArgumentParser(
         description='Learning convnet from ILSVRC2012 dataset')
-    parser.add_argument('--train', default=osp.join(
-        rospack.get_path('decopin_hand'),
-        'train_data', 'dataset', 'train_images.txt'),
-                        help='Path to training image-label list file')
-    parser.add_argument('--val', default=osp.join(
-        rospack.get_path('decopin_hand'),
-        'train_data', 'dataset', 'test_images.txt'),
-                        help='Path to validation image-label list file')
-    parser.add_argument('--arch', '-a', choices=archs.keys(), default='vgg16',
-                        help='Convnet architecture')
-    parser.add_argument('--batchsize', '-B', type=int, default=32,
-                        help='Learning minibatch size')
-    parser.add_argument('--dtype', choices=dtypes, help='Specify the dtype '
-                        'used. If not supplied, the default dtype is used')
     parser.add_argument('--epoch', '-E', type=int, default=10,
                         help='Number of epochs to train')
-    parser.add_argument('--device', '-d', type=str, default='-1',
-                        help='Device specifier. Either ChainerX device '
-                        'specifier or an integer. If non-negative integer, '
-                        'CuPy arrays with specified device id are used. If '
-                        'negative integer, NumPy arrays are used')
-    parser.add_argument('--loaderjob', '-j', type=int,
-                        help='Number of parallel data loading processes')
-    parser.add_argument('--mean', '-m', default=osp.join(
-        rospack.get_path('decopin_hand'),
-        'train_data', 'dataset', 'mean_of_dataset.png'),
-                        help='Mean value of dataset')
-    parser.add_argument('--resume', '-r', default='',
-                        help='Initialize the trainer from given file')
-    parser.add_argument('--out', '-o', default=osp.join(
-        rospack.get_path('decopin_hand'),
-        'scripts', 'result'),
-                        help='Output directory')
-    parser.add_argument('--root', '-R', default=osp.join(rospack.get_path(
-        'decopin_hand'), 'train_data', 'dataset'),
-                        help='Root directory path of image files')
-    parser.add_argument('--val_batchsize', '-b', type=int, default=250,
-                        help='Validation minibatch size')
-    group = parser.add_argument_group('deprecated arguments')
-    group.add_argument('--gpu', '-g', dest='device',
-                       type=int, nargs='?', const=0,
-                       help='GPU ID (negative value indicates CPU)')
+    parser.add_argument('--gpu', '-g', type=int, default=0,
+                        help='GPU ID (negative value indicates CPU)')
+
     args = parser.parse_args()
 
-    import chainer.backends.cuda
-    # device = chainer.get_device(args.device)  # for python3
-    # device = chainer.cuda.get_device(args.device)  # for python2
-    # device = chainer.cuda.get_device_from_id(args.device)  # for python2
-    device = chainer.cuda.get_device_from_id(0)  # for python2
-    # device = chainer.backends.cuda.get_device_from_id(args.device)  # for python2
+    # Configs for train with chainer
+    device = chainer.cuda.get_device_from_id(args.gpu)  # for python2
+    batchsize = 32
+    # Root directory path of image dataset
+    root = osp.join(rospack.get_path(
+        'decopin_hand'), 'train_data', 'dataset')
+    # Output directory of train result
+    out = osp.join(rospack.get_path('decopin_hand'),
+                   'scripts', 'result')
+    # Path to mean image of dataset
+    mean_img_path = osp.join(rospack.get_path('decopin_hand'),
+                             'train_data', 'dataset', 'mean_of_dataset.png')
+    # Path to training image-label list file
+    train_labels = osp.join(rospack.get_path('decopin_hand'),
+                            'train_data', 'dataset', 'train_images.txt')
+    # Path to validation image-label list file
+    val_labels = osp.join(rospack.get_path('decopin_hand'),
+                          'train_data', 'dataset', 'test_images.txt')
 
-    # Set the dtype if supplied.
-    if args.dtype is not None:
-        chainer.config.dtype = args.dtype
-
+    # Initialize the model to train
     print('Device: {}'.format(device))
     print('Dtype: {}'.format(chainer.config.dtype))
-    print('# Minibatch-size: {}'.format(args.batchsize))
+    print('# Minibatch-size: {}'.format(batchsize))
     print('# epoch: {}'.format(args.epoch))
     print('')
 
-    # Initialize the model to train
     n_class = 0
-    with open(osp.join(args.root, 'n_class.txt'), mode='r') as f:
+    with open(osp.join(root, 'n_class.txt'), mode='r') as f:
         for row in f:
             n_class += 1
-    model = archs[args.arch](n_class=n_class)
+    model = VGG16(n_class=n_class)
     model_path = osp.join(rospack.get_path('decopin_hand'), 'scripts', 'VGG_ILSVRC_16_layers.npz')
     if not osp.exists(model_path):
         from chainer.dataset import download
@@ -137,9 +93,6 @@ def main():
         npz.save_npz(model_path, caffemodel, compression=False)
 
     # TODO: add disable_update to original vgg16 layers
-    # TODO: change path of pretrained model
-    # TODO: fix code style
-    from chainer.links import VGG16Layers
     vgg16 = VGG16Layers(pretrained_model=model_path)
     print('Load model from {}'.format(model_path))
     for l in model.children():
@@ -162,18 +115,18 @@ def main():
     device.use()
 
     # Load mean value of dataset
-    mean = np.array(Image_.open(args.mean), np.float32).transpose(
+    mean = np.array(Image_.open(mean_img_path), np.float32).transpose(
         (2, 0, 1))  # (height, width, channel) -> (channel ,height, width), rgb
 
     # Load the dataset files
-    train = PreprocessedDataset(args.train, args.root, mean)
-    val = PreprocessedDataset(args.val, args.root, mean, False)
+    train = PreprocessedDataset(train_labels, root, mean)
+    val = PreprocessedDataset(val_labels, root, mean, False)
     # These iterators load the images with subprocesses running in parallel
     # to the training/validation.
     train_iter = chainer.iterators.MultiprocessIterator(
-        train, args.batchsize, n_processes=args.loaderjob)
+        train, batchsize)
     val_iter = chainer.iterators.MultiprocessIterator(
-        val, args.val_batchsize, repeat=False, n_processes=args.loaderjob)
+        val, batchsize, repeat=False)
     converter = dataset.concat_examples
 
     # Set up an optimizer
@@ -183,7 +136,7 @@ def main():
     # Set up a trainer
     updater = training.updaters.StandardUpdater(
         train_iter, optimizer, converter=converter, device=device)
-    trainer = training.Trainer(updater, (args.epoch, 'epoch'), args.out)
+    trainer = training.Trainer(updater, (args.epoch, 'epoch'), out)
 
     val_interval = 100, 'iteration'
     log_interval = 100, 'iteration'
@@ -206,9 +159,6 @@ def main():
     trainer.extend(extensions.PlotReport(['main/loss', 'validation/main/loss'], x_key='iteration', file_name='loss.png'))
     trainer.extend(extensions.PlotReport(['main/accuracy', 'validation/main/accuracy'], x_key='iteration', file_name='accuracy.png'))
     trainer.extend(extensions.ProgressBar(update_interval=10))
-
-    if args.resume:
-        chainer.serializers.load_npz(args.resume, trainer)
 
     trainer.run()
 
